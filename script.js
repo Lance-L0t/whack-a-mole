b5afc9e822d6b3a9b96a5ec0e648d30f1f1627fb
lancelot <sliceadice@gmail.com>
2026-04-24 16:53:08 +0300

    updaated the css/javascript code

--- script.js
+++ script.js
@@ -1,39 +1,49 @@
 const boxes = document.querySelectorAll(".container");
 const startBtn = document.getElementById("startBtn");
-const timeDisplay = document.getElementById('time');
-const scoreDisplay = document.getElementById('score');
+const timeDisplay = document.getElementById("time");
+const scoreDisplay = document.getElementById("score");
 
 let time = 0;
 let mainLoop = null;
 let score = 0;
-
+const color = 'red';
 
 function startGame() {
-    if (mainLoop !== null) {
-        clearInterval(mainLoop);
-    }
-    score = 0;
-    time = 0;
-    mainLoop = setInterval(function () {
-      let randomBox = Math.floor(Math.random() * 9);
-      boxes[randomBox].style.backgroundColor = "blue";
-      
-      setTimeout(function () {
-        boxes[randomBox].addEventListener("click", function () {
-          score++;
-          scoreDisplay.innerText = `Score: ${score}`;
-          console.log(randomBox);
-        });
-        boxes[randomBox].style.backgroundColor = "black";
-      }, 600);
-      time++;
-      timeDisplay.innerText = `Time: ${time}`;
-      if (time === 30) {
-        clearInterval(mainLoop);
-      }
-    }, 800);
+  if (mainLoop !== null) {
+    clearInterval(mainLoop);
+  }
+  score = 0;
+  time = 0;
+  mainLoop = setInterval(function () {
+
+    //Main Loop
+    let randomBox = Math.floor(Math.random() * 9);
+    boxes[randomBox].style.backgroundColor = color;
+    // Updating the score 
+    updateScore(randomBox);
+    
+    setTimeout(function () {
+      boxes[randomBox].style.backgroundColor = "black";
+    }, 600);
+    time++;
+    timeDisplay.innerText = `Time: ${time}s`;
+    
+  }, 800);
+ if (time === 30) {
+   clearInterval(mainLoop);
+ }
 }
 
 
-
+function updateScore(index){
+  boxes[index].addEventListener("click", function () {
+    if (boxes[index].style.backgroundColor === color) {
+      score++;
+      scoreDisplay.innerText = `Score: ${score}`;
+      console.log(score);
+    } else {
+      removeEventListener("click", updateScore);
+    }
+  });
+}
 
